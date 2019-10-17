5cc37e7de5de2f39954ff95bb3e185615604165c
Fran6nd <francoisnd1@gmail.com>
2019-10-17 18:32:32 +0200

    scene.js: added the concept of uiLayer and layers.

--- scene.js
+++ scene.js
@@ -1,22 +1,31 @@
 class Scene {
     constructor(canvas, ctx) {
         this.id = 0;
-        this.objects = new Map();
+        this.layers = new Array(10);
+        this.uiLayer = new Map();
+        for (var i = 0; i < this.layers.length; i++) {
+            this.layers[i] = new Map();
+        }
         this.canvas = canvas;
         this.ctx = ctx;
         this.t1 = new Date().getTime();
         this.t2 = new Date().getTime();
     }
-    instantiate(obj) {
+    instantiate(obj, layer = 0) {
         obj.id = this.id.toString(10);
-        this.objects.set(obj.id, obj)
+        this.layers[layer].set(obj.id, obj)
         this.id++;
     }
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for (const [key, value] of this.objects.entries()) {
-            value.draw(this.canvas, this.ctx);
+        for (var i = 0; i < this.layers.length; i++) {
+            for (const [key, value] of this.layers[i].entries()) {
+                value.draw(this.canvas, this.ctx);
+            }
         }
+        for (const [key, value] of this.uiLayer.entries()) {
+            value.draw(this.canvas, this.ctx);
+        }       
         this.ctx.font = "15px Arial";
         this.ctx.fillStyle = "#000000";
         this.ctx.fillText("Score: " + new Date().getTime().toString(10), 0, 20);
@@ -24,21 +33,26 @@ class Scene {
     update() {
         this.t2 = new Date().getTime();
         var dt = 1 / (this.t2 - this.t1);
-        for (const [key, value] of this.objects.entries()) {
-            value.update(dt);
+        for (var i = 0; i < this.layers.length; i++) {
+            for (const [key, value] of this.layers[i].entries()) {
+                value.update(dt);
+            }
         }
+        for (const [key, value] of this.uiLayer.entries()) {
+            value.update(dt);
+        } 
         this.t1 = this.t2;
     }
     delete(id) {
-        this.objects.delete(id);
+        for (var i = 0; i < this.layers.length; i++) {
+            this.objects.delete(id);
+        }
     }
-    run()
-    {
+    run() {
         this.update();
         this.draw();
     }
-    start()
-    {
+    start() {
         setInterval(this.run, 10);
     }
 }
